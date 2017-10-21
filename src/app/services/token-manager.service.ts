31ccab148bf81de8743aef0a2bce88108b4423fe
Baboo7 <baptiste.studer@laposte.net>
2017-10-20 18:16:19 -0700

    create token manager

---
+++ src/app/services/token-manager.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class TokenManager {
+
+  private tokenKey: string = 'app_token';
+
+  private store(token: string) {
+    localStorage.setItem(this.tokenKey, token);
+  }
+
+  private retrieve() {
+    let storedToken: string = localStorage.getItem(this.tokenKey);
+    if(!storedToken) throw 'no token found';
+    return storedToken;
+  }
+
+  private remove() {
+    localStorage.removeItem(this.tokenKey);
+  }
+
+  public storeToken(token: string) {
+    this.store(token);
+  }
+
+  public retrieveToken() {
+    let token = null;
+
+    try {
+      let storedToken = this.retrieve();
+      token = storedToken;
+    } catch(err) { }
+
+    return token;
+  }
+
+  public removeToken() {
+    this.remove();
+  }
+}
