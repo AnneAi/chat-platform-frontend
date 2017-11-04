07003146b1e81258c24119ec846b5bfa3c0359e1
Baboo7 <baptiste.studer@laposte.net>
2017-11-03 21:38:33 -0700

    create authentication service

---
+++ src/app/services/authentication.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { TokenManager } from './token-manager.service';
+
+@Injectable()
+export class AuthenticationService {
+
+  constructor(
+    private tokenManager: TokenManager,
+    private router: Router
+  ) { }
+
+  /**************************/
+  /*  interface
+  /**************************/
+
+  /*  Authenticate student and redirect to chat interface.
+
+      PARAMS
+        token (string): token to store in local storage
+
+      RETURN
+        none
+  */
+  public authenticateStudent(token): void {
+    this.tokenManager.storeToken(token);
+    this.router.navigate([ '/student/chat' ]);
+  }
+
+  /*  Disconnect student and redirect to login interface.
+
+      PARAMS
+        none
+
+      RETURN
+        none
+  */
+  public disconnectStudent(): void {
+    this.tokenManager.removeToken();
+    this.router.navigate([ '/student' ]);
+  }
+}
