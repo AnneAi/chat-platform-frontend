780bf6dd14d5f339bfe52efeb33c747e4a3d0f99
Baboo7 <baptiste.studer@laposte.net>
2017-11-03 18:54:13 -0700

    doc token manager service

--- src/app/services/token-manager.service.ts
+++ src/app/services/token-manager.service.ts
@@ -5,20 +5,18 @@ export class TokenManager {
 
   private tokenKey: string = 'app_token';
 
-  private store(token: string) {
-    localStorage.setItem(this.tokenKey, token);
-  }
+  /**************************/
+  /*  interface
+  /**************************/
 
-  private retrieve() {
-    let storedToken: string = localStorage.getItem(this.tokenKey);
-    if(!storedToken) throw 'no token found';
-    return storedToken;
-  }
+  /*  Check if a token exists in the local storage.
 
-  private remove() {
-    localStorage.removeItem(this.tokenKey);
-  }
+      PARAMS
+        none
 
+      RETURN
+        (boolean) true if exists, false otherwise
+  */
   public doesTokenExist(): boolean {
     try {
       this.retrieve();
@@ -28,11 +26,27 @@ export class TokenManager {
     }
   }
 
-  public storeToken(token: string) {
+  /*  Store a token in the local storage.
+
+      PARAMS
+        token (string): token to store
+
+      RETURN
+        none
+  */
+  public storeToken(token: string): void {
     this.store(token);
   }
 
-  public retrieveToken() {
+  /*  Retrieve a token from the local storage.
+
+      PARAMS
+        none
+
+      RETURN
+        (string) retrieved token or null if doesnt exist
+  */
+  public retrieveToken(): string {
     let token = null;
 
     try {
@@ -43,7 +57,57 @@ export class TokenManager {
     return token;
   }
 
+  /*  Remove a token from the local storage.
+
+      PARAMS
+        none
+
+      RETURN
+        none
+  */
   public removeToken() {
     this.remove();
+  }
+
+  /**************************/
+  /*  internal functions
+  /**************************/
+
+  /*  Store a token in the local storage.
+
+      PARAMS
+        token (string): token to store
+
+      RETURN
+        none
+  */
+  private store(token: string): void {
+    localStorage.setItem(this.tokenKey, token);
+  }
+
+  /*  Retrieve a token from the local storage.
+
+      PARAMS
+        none
+
+      RETURN
+        (string) retrieved token
+  */
+  private retrieve(): string {
+    let storedToken: string = localStorage.getItem(this.tokenKey);
+    if(!storedToken) throw 'no token found';
+    return storedToken;
+  }
+
+  /*  Remove a token from the local storage.
+
+      PARAMS
+        none
+
+      RETURN
+        none
+  */
+  private remove(): void {
+    localStorage.removeItem(this.tokenKey);
   }
 }
